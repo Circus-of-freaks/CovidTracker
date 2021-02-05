d136d66898234e4e853affaaafeccb9dacc4fe99
Psina <psiniy-programmer@yandex.ru>
2021-02-05 16:58:18 +0300

    final func

--- src/App/Sidebar/DateCarousel/DateCarousel.tsx
+++ src/App/Sidebar/DateCarousel/DateCarousel.tsx
@@ -1,80 +1,74 @@
 import React, {useState} from 'react'
 import './DateCarousel.css'
 import {days} from "../../../utils/dateDays";
-import arrowLeft from './arrow-left.svg'
-import arrowRight from './arrow_right.svg'
 
 export interface DateCarouselProps {
     dayDate: Date,
     updateDayDate: React.Dispatch<React.SetStateAction<Date>>,
-    dayNumber: number,
-    updateDayNumber: React.Dispatch<React.SetStateAction<number>>
+    pickerDay: number,
+    setPickerDay: React.Dispatch<React.SetStateAction<number>>,
+    dayNumber: number
 }
 
-const DateCarousel = ({dayDate, updateDayDate, dayNumber, updateDayNumber}: DateCarouselProps) => {
+const DateCarousel = ({dayDate, updateDayDate, pickerDay, setPickerDay, dayNumber}: DateCarouselProps) => {
     const [carousel, setCarousel] = useState<number[]>([]);
+    const [selectedNumber, setSelectedNumber] = useState<number>(0);
 
     React.useEffect(() => {
         const setCarouselDays = () => {
             let tempArr: number[] = [];
-            let dayIndex = dayNumber - 1;
-            let tempDate = new Date(dayDate)
-
-            for (let i = 0; i < 7; i++) {
-                tempArr[i] = 0;
-            }
-
-            tempArr[dayIndex] = tempDate.getDate()
+            let dayIndex: number = dayNumber - 1;
+            let tempDate: Date = new Date(dayDate);
 
             for (let i = dayIndex; i >= 0; i--) {
-                tempArr[i] = tempDate.getDate()
-                tempDate.setDate(tempDate.getDate() - 1)
+                tempArr[i] = tempDate.getDate();
+                tempDate.setDate(tempDate.getDate() - 1);
             }
 
             tempDate.setDate(dayDate.getDate())
 
             for (let i = dayIndex; i < 7; i++) {
-                tempArr[i] = tempDate.getDate()
-                tempDate.setDate(tempDate.getDate() + 1)
+                tempArr[i] = tempDate.getDate();
+                tempDate.setDate(tempDate.getDate() + 1);
             }
 
-            setCarousel(tempArr)
+            setCarousel(tempArr);
         }
 
         setCarouselDays()
-    }, [dayDate])
+    }, [dayDate, pickerDay])
 
     return <div className={'sidebar-date'}>
+        <div className="carousel-days">
+            {days.map((dayItem, index) =>
+                <span
+                    key={`dayItem_${index}`}
+                    className={'days-dayName'}>
+                    {dayItem}
+                </span>)}
+        </div>
+        <div className="date-week">
             <span
                 onClick={() => updateDayDate(prevState =>
                     new Date(prevState.setDate(prevState.getDate() - 7)))}
-                className="date-prev"
-            />
-        <div className="date-carousel">
-            {
-                days.map((day, index) =>
-                    <div
-                        key={`${day + String(index)}`}
-                        className={'date-item'}>
-                        <span className={`item-name`}>
-                         {day}
-                         </span>
-                        <span
-                            onClick={() => {
-                                updateDayNumber(index)
-                            }}
-                            className={`item-number`}>
-                        {carousel[index]}
-                    </span>
-                    </div>
-                )
-            }
+                className={'week-prev'}/>
+            <div className="week-numbers">
+                {carousel.map((carouselItem, index) =>
+                    <span
+                        key={`numberItem_${index}`}
+                        onClick={() => {
+                            setPickerDay(index)
+                            setSelectedNumber(index)
+                        }}
+                        className={`numbers-dayNumber ${index === selectedNumber ? 'active' : ''}`}>
+                        {carouselItem}
+                    </span>)}
+            </div>
+            <span
+                onClick={() => updateDayDate(prevState =>
+                    new Date(prevState.setDate(prevState.getDate() + 7)))}
+                className={'week-next'}/>
         </div>
-        <span
-            onClick={() => updateDayDate(prevState =>
-                new Date(prevState.setDate(prevState.getDate() + 7)))}
-            className="date-next"
-        />
     </div>
 }
 
--- src/App/Sidebar/Sidebar.tsx
+++ src/App/Sidebar/Sidebar.tsx
@@ -5,23 +5,21 @@ import ContryStat from "./CountryStat/CountryStat";
 
 const SideBar: React.FC = () => {
     const date = new Date();
-    const [dayDate, setDayDate] = useState(date)
-    const [dayNumber, setDayNumber] = useState(date.getDay())
-
-    React.useEffect(() => {
-        console.log(`CHANGED: ${dayNumber}`)
-    }, [dayNumber])
+    const [dayDate, setDayDate] = useState<Date>(date);
+    const [dayNumber, setDayNumber] = useState<number>(date.getDay());
+    const [pickerDay, setPickerDay] = useState<number>(date.getDay());
 
     return <div className={'sidebar'}>
         <h2 className={'sidebar-header'}>Cases Info</h2>
         <DateCarousel
             dayDate={dayDate}
             updateDayDate={setDayDate}
+            pickerDay={pickerDay}
+            setPickerDay={setPickerDay}
             dayNumber={dayNumber}
-            updateDayNumber={setDayNumber}
         />
         <ContryStat
-            dayNumber={dayNumber}
+            dayNumber={pickerDay}
         />
     </div>
 }
